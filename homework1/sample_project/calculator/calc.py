1ac2dff545cd0093487da4f54b45095a0db07829
natkhosh <t9219788607@gmail.com>
2021-11-06 23:35:19 +0300

    [upd] task1 done

--- homework1/sample_project/calculator/calc.py
+++ homework1/sample_project/calculator/calc.py
@@ -1,2 +1,7 @@
 def check_power_of_2(a: int) -> bool:
-    return not (bool(a & (a - 1)))
+    """
+    Function to find,  if given a positive integer is a power of two or not.
+    :param a: integer
+    :return: bool
+    """
+    return not (bool(a & (a - 1))) and a > 0
--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -1,11 +1,11 @@
 import pytest
 
-from calculator.calc import check_power_of_2
+from homework1.sample_project.calculator.calc import check_power_of_2
 
-def test_positive_case():
+
+@pytest.mark.parametrize(
+    "a, expected_result", [(65536, True), (12, False), (0, False), (1, True)]
+)
+def test_positive_case(a, expected_result):
     """Testing that actual powers of 2 give True"""
-    assert check_power_of_2(65536)
-    
-def test_negative_case():
-    """Testing that non-powers of 2 give False"""
-    assert not check_power_of_2(12)    
+    assert check_power_of_2(a) == expected_result
