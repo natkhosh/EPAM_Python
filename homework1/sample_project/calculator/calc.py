35b242a4e538d5c7d490d40c55e7837269310259
natkhosh <t9219788607@gmail.com>
2021-11-09 22:04:01 +0300

    [upd] task1, made changes

--- homework1/sample_project/calculator/calc.py
+++ homework1/sample_project/calculator/calc.py
@@ -2,6 +2,6 @@ def check_power_of_2(a: int) -> bool:
     """
     Function to find,  if given a positive integer is a power of two or not.
     :param a: integer
-    :return: bool
+    :return: bool: true for success if the given integer is a power of two, False otherwise.
     """
     return not (bool(a & (a - 1))) and a > 0
