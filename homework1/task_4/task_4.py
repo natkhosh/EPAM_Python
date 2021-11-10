77d25287dcfe01a52ff1137775f230d99fd146f4
natkhosh <t9219788607@gmail.com>
2021-11-10 23:11:51 +0300

    [upd] task4, Done

--- homework1/task_4/task_4.py
+++ homework1/task_4/task_4.py
@@ -10,17 +10,25 @@ from typing import List
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
-    ...
+    """
+    Function compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+    :param a , b, c, d:  list of integer values, have same length of N where 0 ≤ N ≤ 1000
+    :return: integer, amount of found tuples with sum of items is zero
+    """
+    sums = {}
+    for i in a:
+        for j in b:
+            if i + j not in sums:
+                sums[i + j] = 1
+            else:
+                sums[i + j] += 1
+    counter = 0
+    for i in c:
+        for j in d:
+            if -1 * (i + j) in sums:
+                # print(-1 * (i+j))
+                counter += sums[-1 * (i + j)]
+        return counter
 
 
-# arr1 = [1, 2]
-# arr2 = [3, 5]
-# sum_ = []
-# for i in range(len(arr1)):
-#     for j in range(len(arr1)):
-#         sum_.append(arr1[i] + arr2[j])
-#         print(sum_)
-
-#
-# def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
-#     ...
+print(check_sum_of_four([0, 1], [-1, 0], [1, 0], [0, -1]))
