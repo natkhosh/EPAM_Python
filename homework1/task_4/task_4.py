06aa28ee8d4c29efb7b95925590cd13cc365d294
natkhosh <t9219788607@gmail.com>
2021-11-10 23:16:26 +0300

    [upd] task4, modified docstring

--- homework1/task_4/task_4.py
+++ homework1/task_4/task_4.py
@@ -12,7 +12,10 @@ from typing import List
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
     """
     Function compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
-    :param a , b, c, d:  list of integer values, have same length of N where 0 ≤ N ≤ 1000
+    :param a: list of integer values, have same length of N where 0 ≤ N ≤ 1000
+    :param b: list of integer values, have same length of N where 0 ≤ N ≤ 1000
+    :param c: list of integer values, have same length of N where 0 ≤ N ≤ 1000
+    :param d: list of integer values, have same length of N where 0 ≤ N ≤ 1000
     :return: integer, amount of found tuples with sum of items is zero
     """
     sums = {}
@@ -26,9 +29,5 @@ def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) ->
     for i in c:
         for j in d:
             if -1 * (i + j) in sums:
-                # print(-1 * (i+j))
                 counter += sums[-1 * (i + j)]
         return counter
-
-
-print(check_sum_of_four([0, 1], [-1, 0], [1, 0], [0, -1]))
