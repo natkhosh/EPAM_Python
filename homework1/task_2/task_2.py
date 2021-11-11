e35a246f5eb7b175e51b77afd14c153bd0f8b23c
natkhosh <t9219788607@gmail.com>
2021-11-11 23:24:03 +0300

    [upd] task2, changed the algorithm

--- homework1/task_2/task_2.py
+++ homework1/task_2/task_2.py
@@ -5,18 +5,28 @@ returns if the given sequence is a Fibonacci sequence
 
 We guarantee, that the given sequence contain >= 0 integers inside.
 """
+import math
 from typing import Sequence
 
 
-def _check_window(x: int, y: int, z: int) -> bool:
+def isPerfectSquare(x: int) -> bool:
     """
-    Support function to checking sum
-    :param x: integer
-    :param y: integer
-    :param z: integer
-    :return: bool
+    A utility function that returns true if x is perfect square
+    :param x: integer number
+    :return: bool: true for success if x is perfect square, False otherwise.
     """
-    return (x + y) == z
+    s = int(math.sqrt(x))
+    return s * s == x
+
+
+def isFibonacci(n: int) -> bool:
+    """
+    Function checking if a number is Fibonacci.
+    n is Fibonacci if one of 5*n*n + 4 or 5*n*n - 4 or both is a perferct square.
+    :param n: integer number to check
+    :return: bool:Returns true if n is a Fibonacci Number, else false
+    """
+    return isPerfectSquare(5 * n * n + 4) or isPerfectSquare(5 * n * n - 4)
 
 
 def check_fibonacci(data: Sequence[int]) -> bool:
@@ -25,15 +35,22 @@ def check_fibonacci(data: Sequence[int]) -> bool:
     :param data: Sequence of integers
     :return: bool: true for success if the given sequence is a Fibonacci sequence, False otherwise.
     """
-    if len(data) < 3:
-        print("Invalid data")
+    if len(data) < 2:
         return False
-    else:
-        while len(data) >= 3:
-            x, y, z = data[0], data[1], data[2]
-
-            if _check_window(x, y, z):
-                data = data[1:]
-            else:
+    elif len(data) < 3:
+        for i in range(1, len(data)):
+            if not (
+                (data[i - 1] <= data[i])
+                and isFibonacci(data[i - 1])
+                and isFibonacci(data[i])
+                and isFibonacci(data[i - 1] + data[i])
+            ):
                 return False
         return True
+    else:
+        if (data[0] < data[1]) and isFibonacci(data[0]) and isFibonacci(data[1]):
+            for i in range(2, len(data)):
+                if data[i] != data[i - 1] + data[i - 2]:
+                    return False
+            return True
+        return False
--- homework1/tests/test_task2.py
+++ homework1/tests/test_task2.py
@@ -8,9 +8,13 @@ from homework1.task_2.task_2 import check_fibonacci
 @pytest.mark.parametrize(
     "value, expected_result",
     [
-        ([0, 1], False),
+        ([0, 1], True),
+        ([1, 1], True),
+        ([2, 1], False),
+        ([2, 3], True),
         ([1], False),
         ([], False),
+        ([10, -5, 5], False),
         ([0, 1, 2, 3, 5, 8], False),
         ([0, 1, 1, 2, 3, 5, 8], True),
         ([377, 610, 987, 1597], True),
