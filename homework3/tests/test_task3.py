5e4d71ae9aa7d598e1aa5978dc575562399db317
natkhosh <t9219788607@gmail.com>
2021-11-18 15:21:31 +0300

    [upd] hw3, test3 - modified docstring

--- homework3/tests/test_task3.py
+++ homework3/tests/test_task3.py
@@ -33,5 +33,6 @@ def test_make_filter(
     kwargs, some_data: List[Any], expected_result: List[Any]
 ):
     """
+    Testing that function generate filter object for specified keywords.
     """
     assert make_filter(**kwargs).apply(some_data) == expected_result
