446a32b380cd99d61314d5daf68d7167c8961f58
natkhosh <t9219788607@gmail.com>
2021-11-18 15:33:26 +0300

    [upd] hw3, task 1 + test --> Done

--- homework3/task_1/task_1.py
+++ homework3/task_1/task_1.py
@@ -16,15 +16,51 @@ Example::
         return input('? ')   # careful with input() in python2, use raw_input()
         instead
 
-    >>> f()
+    ''>>> f()
     ? 1
     '1'
-    >>> f()     # will remember previous value
+    ''>>> f()     # will remember previous value
     '1'
-    >>> f()     # but use it up to two times only
+    ''>>> f()     # but use it up to two times only
     '1'
-    >>> f()
+    ''>>> f()
     ? 2
     '2'
 
 """
+from typing import Callable
+
+
+def cache(times: int) -> Callable:
+    """
+    Remembers other function output value. Gives out cached value up to "times"
+    number only.
+    :param times: quantity of returned cached values.
+    :return: Wrapper around recieved functon with cache functionality.
+    """
+
+    def cache_func(func: Callable) -> Callable:
+        """
+        Functtion accepts another function as an argument
+        :param func: input function
+        :return: such a function, so the every call to initial one should be
+        cached
+        """
+        counter = 0
+        cache_dict = {}
+
+        def wrapper(*args):
+            nonlocal counter, cache_dict
+
+            if counter == times:
+                cache_dict.pop(args, None)
+                counter = 0
+
+            if args not in cache_dict:
+                cache_dict[args] = func(*args)
+            else:
+                counter += 1
+            return cache_dict[args]
+
+        return wrapper
+    return cache_func
--- homework3/tests/test_task1.py
+++ homework3/tests/test_task1.py
@@ -1,3 +1,19 @@
-import pytest
+from homework3.task_1.task_1 import cache
 
-import homework3.task_1.task_1
+
+def test_cache():
+    """
+    Testing that function remembers other function output value.
+    Gives out cached value up to "times" number only.
+    """
+    @cache(times=2)
+    def func(a, b):
+        return a ** b ** 2
+
+    cache_res_1 = func(5, 2)
+    cache_res_2 = func(5, 2)
+    cache_res_3 = func(5, 2)
+    cache_res_4 = func(8, 2)
+
+    assert cache_res_1 == cache_res_2 == cache_res_3
+    assert cache_res_4 != cache_res_3
