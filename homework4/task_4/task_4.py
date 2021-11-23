02e003b88e1b17fdd9af56b56ee107e41729bd0a
natkhosh <t9219788607@gmail.com>
2021-11-23 17:53:29 +0300

    [upd] hw4, task_4 + doctest --> Done

---
+++ homework4/task_4/task_4.py
@@ -0,0 +1,71 @@
+"""
+Write a function that takes a number N as an input and returns N FizzBuzz
+numbers*
+Write a doctest for that function.
+
+Definition of done:
+ - function is created
+ - function is properly formatted
+ - function has doctests
+ - doctests are run with pytest command
+
+You will learn:
+ - the most common test task for developers
+ - how to write doctests
+ - how to run doctests
+
+
+assert fizzbuzz(5) == ["1", "2", "fizz", "4", "buzz"]
+
+* https://en.wikipedia.org/wiki/Fizz_buzz
+** Энциклопедия профессора Фортрана page 14, 15, "Робот Фортран, чисть
+картошку!"
+"""
+from typing import List
+
+
+def fizzbuzz(n: int) -> List[str]:
+    """
+    Function takes a number N as an input and returns N FizzBuzz numbers.
+    Any number divisible by three is replaced by the word fizz.
+    Any number divisible by five by the word buzz.
+    Numbers divisible by 15 become fizz buzz.
+    :param n: length of Fizzbuzz sequence
+    :return: list: Fizzbuzz sequence
+
+    >>> fizzbuzz(5)
+    ['1', '2', 'fizz', '4', 'buzz']
+    >>> fizzbuzz(1)
+    ['1']
+    >>> fizzbuzz(0)
+    Traceback (most recent call last):
+    ...
+    ValueError: n must be not less than 1
+    >>> fizzbuzz(1.5)
+    Traceback (most recent call last):
+    ...
+    ValueError: n must be integer
+
+    """
+    if n < 1:
+        raise ValueError("n must be not less than 1")
+    if isinstance(n, float):
+        raise ValueError("n must be integer")
+
+    result = []
+    for number in range(1, n + 1):
+        if number % 3 == 0:
+            result.append("fizz")
+        elif number % 5 == 0:
+            result.append("buzz")
+        elif number % 15 == 0:
+            result.append("fizz buzz")
+        else:
+            result.append(str(number))
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
