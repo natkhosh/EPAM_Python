2b672b1dd3192b6497250d4baf9351aa9e3b4253
natkhosh <t9219788607@gmail.com>
2021-12-08 20:48:52 +0300

    [upd] hw7 , task_1 + test -> Done

--- homework7/task_1/task_1.py
+++ homework7/task_1/task_1.py
@@ -7,26 +7,18 @@ Tree can only contains basic structures like:
 """
 from typing import Any
 
-# Example tree:
-example_tree = {
-    "first": ["RED", "BLUE"],
-    "second": {
-        "simple_key": ["simple", "list", "of", "RED", "valued"],
-    },
-    "third": {
-        "abc": "BLUE",
-        "jhl": "RED",
-        "complex_key": {
-            "key1": "value1",
-            "key2": "RED",
-            "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
-        }
-     },
-    "fourth": "RED",
-}
-
 
 def find_occurrences(tree: dict, element: Any) -> int:
+    """
+    Function takes element and finds the number of counter of this element in
+    the tree.
+    Tree can only contains basic structures like:
+    str, list, tuple, dict, set, int, bool
+    :param tree: dictionary
+    :param element: any basic structures
+    (str, list, tuple, dict, set, int, bool)
+    :return: integer, number of occurrences in the tree.
+    """
     counter = 0
     if isinstance(tree, dict):
         tree = tree.values()
@@ -39,7 +31,3 @@ def find_occurrences(tree: dict, element: Any) -> int:
         else:
             counter += find_occurrences(value, element)
     return counter
-
-
-if __name__ == '__main__':
-    print(find_occurrences(example_tree, "RED"))  # 6
