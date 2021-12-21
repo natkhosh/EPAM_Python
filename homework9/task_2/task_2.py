8f16be7bcb5c1740267e6b9c85b1f67c5bf59c20
natkhosh <t9219788607@gmail.com>
2021-12-21 11:48:17 +0300

    hw9: task1, task2, task3 -> Done

---
+++ homework9/task_2/task_2.py
@@ -0,0 +1,46 @@
+"""
+Write a context manager, that suppresses passed exception.
+Do it both ways: as a class and as a generator.
+>> with supressor(IndexError):
+...    [][2]
+"""
+from contextlib import contextmanager
+
+
+class Supressor:
+    """
+    Сontext manager, that suppresses passed exception.
+    """
+
+    def __init__(self, exception):
+        """
+        Method to initialize the object’s attributes
+        :param exception:  any exception
+        """
+        self.exception = exception
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return True if self.exception is exc_type else False
+
+
+@contextmanager
+def supressor(exception):
+    """
+    Сontext manager, that suppresses passed exception.
+    :param exception:  any exception
+    """
+    try:
+        yield
+    except exception:
+        pass
+
+
+if __name__ == "__main__":
+    # with Supressor(IndexError):
+    #     [][2]
+
+    with supressor(IndexError):
+        [][2]
---
+++ homework9/task_3/task_3.py
@@ -0,0 +1,49 @@
+"""
+Write a function that takes directory path, a file extension and an optional
+tokenizer.
+It will count lines in all files with that extension if there are no tokenizer.
+If a the tokenizer is not none, it will count tokens.
+For dir with two files from hw1.py:
+>> universal_file_counter(test_dir, "txt")
+6
+>> universal_file_counter(test_dir, "txt", str.split)
+6
+"""
+from pathlib import Path, PureWindowsPath
+from typing import Callable, Optional, Union
+
+
+def universal_file_counter(
+        dir_path: Union[str, Path], file_extension: str,
+        tokenizer: Optional[Callable] = None
+) -> int:
+    """
+    This function count lines in all files with that extension
+    if there are no tokenizer.
+    If a the tokenizer is not none, it will count tokens.
+    :param dir_path: path to directory
+    :param file_extension: extension of files
+    :param tokenizer: string method
+    :return: integer, count of lines or tokens
+    """
+    count = 0
+    path = Path(dir_path)
+
+    if not path.is_dir():
+        raise OSError('Directory not found')
+
+    files_list = path.glob(f'*{file_extension}')
+
+    for fn in files_list:
+        with open(PureWindowsPath(path).joinpath(fn), "r") as f:
+            for line in f:
+                if tokenizer:
+                    count += len(tokenizer(line))
+                else:
+                    count += 1
+    return count
+
+
+if __name__ == "__main__":
+    print(universal_file_counter('../tests', ".txt"))
+    print(universal_file_counter('../tests', ".txt", str.split))
