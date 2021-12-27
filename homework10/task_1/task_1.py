446b42cffee5429d48b69ddee47dad03dac0d6f3
natkhosh <t9219788607@gmail.com>
2021-12-27 22:40:53 +0300

    hw10: base parsing

--- homework10/task_1/task_1.py
+++ homework10/task_1/task_1.py
@@ -33,3 +33,96 @@ P/E компании (информация находится справа от 
 For scrapping you cans use beautifulsoup4
 For requesting aiohttp
 """
+# import asyncio
+import datetime
+import re
+from collections import defaultdict
+
+# import aiohttp
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://markets.businessinsider.com/index/components/s&p_500"
+COMPANY_INFO = defaultdict(dict)
+
+
+def current_usd_rate():
+    data = datetime.datetime.now()
+    url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" \
+          + data.strftime('%d/%m/%Y')
+
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "lxml")
+    return float(soup.find("valute", attrs={"id": "R01235"}).
+                 value.text.replace(",", "."))
+
+
+def get_last_page(url):
+    # url = "https://markets.businessinsider.com/index/components/s&p_500"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    num_pages = soup.find('div', class_="finando_paging margin-top--small").\
+        find_all("a")
+    return int(num_pages[-2].text)
+
+
+def company_list():
+    for i in range(1, 2):
+        url = f"https://markets.businessinsider.com/index/" \
+              f"components/s&p_500?p={i}"
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        table_body = soup.find("tbody", class_="table__tbody")
+        table_rows = table_body.find_all("tr")
+
+        for row in table_rows:
+            href = row.find("a")["href"]
+            company_url = "https://markets.businessinsider.com" + href
+            code = re.split(r"/", href)[-1].split("-")[0].upper()
+            name = row.find("a").text.strip()
+            growth = float(row.find_all("td")[-1].find_all("span")[-1].
+                           text[:-1])
+
+            COMPANY_INFO[company_url]["code"] = code
+            COMPANY_INFO[company_url]["name"] = name
+            COMPANY_INFO[company_url]["growth"] = growth
+
+
+def company_info():
+
+    for company_url in COMPANY_INFO.keys():
+        res = requests.get(company_url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        try:
+            pe_value = soup.find("div", string="P/E Ratio").parent.\
+                find(text=True).strip().replace(',', '')
+        except AttributeError:
+            pe_value = None
+
+        COMPANY_INFO[company_url]["P/E"] = pe_value
+
+#
+# def get_potential_profit(soup) -> float:
+#
+#     week_high_element = soup.find(
+#         class_="snapshot__header", text="52 Week High")
+#     week_low_element = soup.find(
+#         class_="snapshot__header", text="52 Week Low")
+#     if week_low_element is None and week_high_element is None:
+#         profit = 0
+#     else:
+#         week_high_value = week_high_element.findParent().text.split()[0]
+#         week_high = float(week_high_value.replace(",", ""))
+#
+#         week_low_value = week_low_element.findParent().text.split()[0]
+#         week_low = float(week_low_value.replace(",", ""))
+#         potential_profit = week_high / week_low
+#
+#         profit = round(potential_profit, 2)
+#     return profit
+
+
+if __name__ == "__main__":
+    company_list()
+    company_info()
+    print(COMPANY_INFO)
