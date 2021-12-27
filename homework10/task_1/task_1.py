43fc10ba91668d8a3868da968b378a00ffc09d08
natkhosh <t9219788607@gmail.com>
2021-12-27 23:58:17 +0300

    hw10: add parsing functions, add json export, parsing just one page without asyncio

--- homework10/task_1/task_1.py
+++ homework10/task_1/task_1.py
@@ -35,8 +35,10 @@ For requesting aiohttp
 """
 # import asyncio
 import datetime
+import json
 import re
 from collections import defaultdict
+from heapq import nlargest, nsmallest
 
 # import aiohttp
 import requests
@@ -67,6 +69,7 @@ def get_last_page(url):
 
 
 def company_list():
+    rub = current_usd_rate()
     for i in range(1, 2):
         url = f"https://markets.businessinsider.com/index/" \
               f"components/s&p_500?p={i}"
@@ -82,47 +85,84 @@ def company_list():
             name = row.find("a").text.strip()
             growth = float(row.find_all("td")[-1].find_all("span")[-1].
                            text[:-1])
+            price = float(row.find_all("td")[1].text.split()[0].
+                          strip().replace(',', '')) * rub
 
             COMPANY_INFO[company_url]["code"] = code
             COMPANY_INFO[company_url]["name"] = name
             COMPANY_INFO[company_url]["growth"] = growth
+            COMPANY_INFO[company_url]["price"] = price
 
 
-def company_info():
+def get_pe_ratio():
 
     for company_url in COMPANY_INFO.keys():
         res = requests.get(company_url)
         soup = BeautifulSoup(res.text, 'html.parser')
         try:
-            pe_value = soup.find("div", string="P/E Ratio").parent.\
-                find(text=True).strip().replace(',', '')
+            pe_value = float(soup.find("div", string="P/E Ratio").parent
+                             .find(text=True).strip().replace(',', ''))
         except AttributeError:
-            pe_value = None
+            pe_value = 0
 
         COMPANY_INFO[company_url]["P/E"] = pe_value
 
-#
-# def get_potential_profit(soup) -> float:
-#
-#     week_high_element = soup.find(
-#         class_="snapshot__header", text="52 Week High")
-#     week_low_element = soup.find(
-#         class_="snapshot__header", text="52 Week Low")
-#     if week_low_element is None and week_high_element is None:
-#         profit = 0
-#     else:
-#         week_high_value = week_high_element.findParent().text.split()[0]
-#         week_high = float(week_high_value.replace(",", ""))
-#
-#         week_low_value = week_low_element.findParent().text.split()[0]
-#         week_low = float(week_low_value.replace(",", ""))
-#         potential_profit = week_high / week_low
-#
-#         profit = round(potential_profit, 2)
-#     return profit
+
+def get_potential_profit():
+    for company_url in COMPANY_INFO.keys():
+        res = requests.get(company_url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        week_high_element = soup.find(
+            class_="snapshot__header", text="52 Week High")
+        week_low_element = soup.find(
+            class_="snapshot__header", text="52 Week Low")
+        if week_low_element is None and week_high_element is None:
+            profit = 0
+        else:
+            week_high_value = week_high_element.findParent().text.split()[0]
+            week_high = float(week_high_value.replace(",", ""))
+
+            week_low_value = week_low_element.findParent().text.split()[0]
+            week_low = float(week_low_value.replace(",", ""))
+            potential_profit = week_high / week_low
+
+            profit = round(potential_profit, 2)
+        COMPANY_INFO[company_url]["potential profit"] = profit
+
+
+def save_to_json(filename, value_name, data):
+    with open(filename + ".json", "w") as file:
+        top_10 = [
+            {
+                "name": data[i]["name"],
+                "code": data[i]["code"],
+                f"{value_name}": data[i][value_name],
+            }
+            for i in range(10)
+        ]
+        json.dump(top_10, file, indent=4)
+
+
+def top_10_companies():
+    save_to_json("price", "price", nlargest(10,
+                                            COMPANY_INFO.values(),
+                                            key=lambda x: x["price"]))
+    save_to_json("pe", "P/E", nsmallest(10,
+                                        COMPANY_INFO.values(),
+                                        key=lambda x: x["P/E"]))
+    save_to_json("growth", "growth", nlargest(10,
+                                              COMPANY_INFO.values(),
+                                              key=lambda x: x["growth"]))
+    save_to_json("profit", "potential profit",
+                 nlargest(10,
+                          COMPANY_INFO.values(),
+                          key=lambda x: x["potential profit"]))
 
 
 if __name__ == "__main__":
     company_list()
-    company_info()
-    print(COMPANY_INFO)
+    get_pe_ratio()
+    get_potential_profit()
+    # print(COMPANY_INFO)
+    top_10_companies()
